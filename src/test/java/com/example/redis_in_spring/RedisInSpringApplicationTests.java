928ca8beb6ecc328c187ccfe30a6daa2a58fb904
rmsoft-phil <sdp@rmsoft.kr>
2024-09-10 03:04:21 +0900

    first commit

---
+++ src/test/java/com/example/redis_in_spring/RedisInSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redis_in_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisInSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
